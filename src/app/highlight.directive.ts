e0a8c0ee84503dc7885ccac0159a4a088166e731
pphan <philipp.phan@adesso.de>
2024-11-19 10:05:32 +0100

    19112024

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
   standalone: true
 })
 export class HighlightDirective {
-  
+
+  constructor(private el: ElementRef) {
+    this.el.nativeElement.style.backgroundColor = '';
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
